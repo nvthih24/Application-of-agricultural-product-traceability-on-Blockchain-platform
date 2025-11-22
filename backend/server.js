1be4c5c5577f83329d0539589ca96064a29157af
Baozxje <lamgiabao73@gmail.com>
2025-11-22 13:39:53 +0700

    load lai be

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,21 @@ const qrCodeRoutes = require('./routes/qrcodes');
 dotenv.config();
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors());
+
+// Routes
 app.use('/api/upload', uploadRoutes);
 app.use('/api/qrcodes', qrCodeRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use("/api/auth", authRoutes);
-
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
